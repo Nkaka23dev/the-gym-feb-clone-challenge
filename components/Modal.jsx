2e1b4885f57ee3f905053cac43600dd90703d25b
Nkaka23dev <nkakaeric96@gmail.com>
2023-02-20 16:25:03 +0200

    Done with the menu

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import Link from "next/link";
+import React from "react"; 
 
-export default function Modal({setShow}) {
+export default function Modal({ setShow }) {
   const handleClick = () => {
     setShow(false);
   };
   return (
     <section className="bg-white h-screen z-50 max-w-7xl mx-auto fixed grid inset-0">
-      <div onClick={handleClick} className="bg-[#F2EFED] ml-auto p-5 cursor-pointer absolute top-0 right-0">
+      <div
+        onClick={handleClick}
+        className="bg-[#F2EFED] ml-auto p-5 cursor-pointer absolute top-0 right-0"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className=""
@@ -20,13 +24,54 @@ export default function Modal({setShow}) {
           <line x1="48" y1="16" x2="16" y2="48" />
         </svg>
       </div>
-      <div className="grid grid-cols-2">
-        <div className="flex flex-col space-y-10 pt-52 px-36">
-          {[1, 2, 3, 4, 5, 6, 7].map((e, i) => {
+      <div className="">
+        <div className="flex justify-between   flex-col space-y-10 pt-52 cursor-pointer  px-36">
+          {["Study at VU Amsterdam", "Research", "About VU Amsterdam", "VU press office", "Alumni", "Student", "Employee"].map((e, i) => {
             return (
-              <div>
-                <p className="text-2xl font-semibold"> Study at VU Amsterdam</p>
+              <Link href={e}>
+              <div key={i} className="flex max-w-sm  justify-between">
+                <p className="text-[1.4rem] whitespace-nowrap font-semibold">
+                  {" "}
+                 {e}
+                </p>
+                {i === 0 || i === 1 ? (
+                  <svg
+                    
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="1.1"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-plus"
+                  >
+                    <line x1="12" y1="5" x2="12" y2="19" />
+                    <line x1="5" y1="12" x2="19" y2="12" />
+                  </svg>
+                ) : (
+                  <svg
+                    className="ml-36"
+                    Name="fill-[#000000]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30px"
+                    height="30px"
+                    viewBox="0 0 24 24"
+                    fill="#000000"
+                  >
+                    <path
+                      d="M20 12L4 12M20 12L14 18M20 12L14 6"
+                      stroke="#000000"
+                      stroke-width="1.1"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                )}
               </div>
+              </Link>
             );
           })}
         </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,8 +6,8 @@ export default function Navbar() {
   const [show, setShow] = useState(false);
 
   const handClick = () => {
-    setShow(true)
-  }
+    setShow(true);
+  };
   return (
     <section className="px-5 ">
       <div className="max-w-[78rem] fixed z-20 left-0 right-0 top-0 shade bg-white mx-auto  flex justify-between items-stretch">
@@ -18,14 +18,24 @@ export default function Navbar() {
             alt=""
           />
         </div>
-        <div className="flex py-5 px-6 gap-6 items-center cursor-pointer bg-[#F2EFED] ">
+        <div
+          onClick={handClick}
+          className="flex py-5 px-6  gap-6 items-center cursor-pointer bg-[#F2EFED] "
+        >
           {data.map((value, index) => {
-            return <div onClick={handClick} className={`${index === 0? "text-xl font-medium":''}`} key={index}>{value}</div>;
+            return (
+              <div
+                onClick={handClick}
+                className={`${index === 0 ? "text-xl font-medium" : ""}`}
+                key={index}
+              >
+                {value}
+              </div>
+            );
           })}
         </div>
       </div>
-      {show && <Modal setShow={setShow}/>}
+      {show && <Modal setShow={setShow} />}
     </section>
-
   );
 }
